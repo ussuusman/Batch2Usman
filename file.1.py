b31ca03e05bc78fcc40d7f413bb686c49ec5c106
ussuusman <162982291+ussuusman@users.noreply.github.com>
2024-03-11 15:57:42 +0530

    Add files via upload

---
+++ file.1.py
@@ -0,0 +1,84 @@
+'''
+ Bargained characters
+
+  Controls and data structure-->ABC LANGUAGE
+  Strings and its format-->from C
+  Regular expressions-->Pearl
+  Exception handling-->Module 3
+  Magic methods from-->fortron
+
+
+   >What kind of language is python?
+
+python is a
+
+    object oriented programing language
+    dynamically typed language
+    scripting language
+    interepted language
+    compiled language
+
+    *FLAVOURS OF PYTHON:-
+
+>Cpython - Written in C, Most common Implementation of python.
+>Jython - Java implementation of Python.
+>IronPython - implementation of the Python programming language which is tightly integrated with .NET
+>Brython - Browser Python, runs in browsers
+>A RubyPython - Bridge between Python and Ruby Interpreters
+>MicroPython - Runs in Micro Controllers
+
+
+
+#--->print(object,sep,end,file,flush)
+print has 5 parameters
+# object -->the value to be printed is object
+# sep--> seperator
+# Eg
+# a = 8
+# b = 9
+# c = 89
+# print(a, b,c, sep='$')
+# end --> which is used to add the string at the end of the value,
+# # It will not keep the command to stay in same line
+# Eg:
+# print(str(a), b, end='%%')
+# print(str(c))
+
+
+
+#!----> variables
+#?variable is a name which is used to identify the value of variable
+# ?which is stored in a specific memory location
+#! Rules to dedfine variables
+#1.) Variable have to start with only alphabets, underscores
+#2.) The char following the 1st char can be alphabets, numbers, underscores
+#3.) No spaces are allowed
+#4.) No special characters like !@#$%^&*()+
+#5.) case sensitive
+#6.) Keywords are not allowed as variables
+
+
+
+  #print("hello")
+
+
+print("hello world")
+print("hello world")
+print("hello world")
+
+1).Commenting is used for making documentation in a python file.
+2).When the line is commented , that line will not be considered as a part of the code.
+3).When the program is exucuted, commented line will not be in the part of execution.
+
+------> NOTE
+Single line commenting can be done using #
+'''
+#print("hello world")
+#print("hello world")
+#import this
+#print(this)
+
+
+# import is a keybord which is used to communicated between the python files.
+import antigravity
+
